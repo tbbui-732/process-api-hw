cc6991e5937f82f5fd2a7a7f0afa48e97e16b504
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-08-02 19:40:08 -0400

    throw fork program into a function

--- main.c
+++ main.c
@@ -3,15 +3,33 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+/* CUSTOM TYPES */
+typedef enum { FALSE, TRUE } bool_t;
+
+/* FORWARD DECLARATIONS */
+bool_t change_value(const pid_t pid);
+
 int main() {
+    pid_t pid = fork();
+    bool_t change_value_state = change_value(pid);
+    if (!change_value_state) {
+        perror("unable to fork program");
+    }
+    return 0;
+}
+
+
+/* FUNCTION DEFINITIONS */
+bool_t change_value(const pid_t pid) {
     int shared_value = 100;
     const int CHILD_VALUE = 50;
     const int PARENT_VALUE = 75;
-    pid_t pid = fork();
-    if (pid < 0) { /* failure */
-        perror("unable to fork program");
-        return EXIT_FAILURE;
-    } else if (pid == 0) { /* child */
+
+    if (pid < 0) { /* failure, just like you*/
+        return FALSE;
+    }
+
+    if (pid == 0) { /* child */
         printf("child before: %d\n", shared_value);
         shared_value += CHILD_VALUE;
         printf("child after: %d (expecting 150)\n", shared_value);
@@ -20,5 +38,6 @@ int main() {
         shared_value += PARENT_VALUE;
         printf("parent after: %d (expecting 175)\n", shared_value);
     }
-    return 0;
+
+    return TRUE;
 }

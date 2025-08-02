e941fe04a92c96bfe65842032346526085398c40
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-08-02 19:18:32 -0400

    add fork structure

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main() {
-    printf("hey there");
+    pid_t pid = fork(); 
+    if (pid < 0) { // failure
+        perror("unable to fork program");
+        return EXIT_FAILURE;
+    } else if (pid == 0) { // child
+    } else { // parent
+    }
     return 0;
 }

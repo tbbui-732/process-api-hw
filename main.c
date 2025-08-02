7c902f460813206497d37372f33c2d07fe9c84dd
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-08-02 19:25:37 -0400

    create 'global' variable for parent and child
    parent and child will print the current value and make their own changes
    to it

--- main.c
+++ main.c
@@ -4,12 +4,21 @@
 #include <unistd.h>
 
 int main() {
-    pid_t pid = fork(); 
-    if (pid < 0) { // failure
+    int shared_value = 100;
+    const int CHILD_VALUE = 50;
+    const int PARENT_VALUE = 75;
+    pid_t pid = fork();
+    if (pid < 0) { /* failure */
         perror("unable to fork program");
         return EXIT_FAILURE;
-    } else if (pid == 0) { // child
-    } else { // parent
+    } else if (pid == 0) { /* child */
+        printf("child before: %d\n", shared_value);
+        shared_value += CHILD_VALUE;
+        printf("child after: %d (expecting 150)\n", shared_value);
+    } else { /* parent */
+        printf("parent before: %d\n", shared_value);
+        shared_value += PARENT_VALUE;
+        printf("parent after: %d (expecting 175)\n", shared_value);
     }
     return 0;
 }
